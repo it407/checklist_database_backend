63db063be758eaedf20efba46f6f999da7efb1ed
Ankitkumar-dewangan <ankitdewangan1122@gmail.com>
2026-02-11 14:53:30 +0530

    checklist backend final

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { login } from "../controllers/auth.controller.js";
 
 import { getProfile } from '../controllers/auth.controller.js';
-import { verifyToken } from '../middleware/auth.middleware.js';
+import { verifyToken } from '../middlewares/auth.middleware.js';
 
 
 const router = express.Router();
